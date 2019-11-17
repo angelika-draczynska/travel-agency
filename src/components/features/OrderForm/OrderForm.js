f6cc87ef903af58e6ff87cbae34ed27135e30814
Angelika Draczynska <nayrissa@outlook.com>
2019-11-17 15:48:27 +0100

    complete API data and validation

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -9,53 +9,93 @@ import { calculateTotal } from '../../../utils/calculateTotal';
 import { formatPrice } from '../../../utils/formatPrice';
 import Button from '../../common/Button/Button';
 
-const sendOrder = (options, tripCost, tripId, tripName, countryCode) => {
-  const totalCost = formatPrice(calculateTotal(tripCost, options));
-
-  const payload = {
-    ...options,
-    totalCost,
-    tripId,
-    tripName,
-    countryCode,
+class OrderForm extends React.Component {
+  state = {
+    name: '',
+    phone: '',
   };
 
-  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+  validateForm(data) {
+    console.log('data', data);
+    if (!data.name) {
+      this.setState({ error: 'Name field is required' });
+      return false;
+    }
+    this.setState({ error: null });
+    return true;
+  }
 
-  const fetchOptions = {
-    cache: 'no-cache',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  };
+  sendOrder(options, tripCost, tripId, tripName, countryCode) {
+    // const { options, setOrderOption, tripCost, tripId, tripName, countryCode } = this.props;
+    const isValid = this.validateForm(options);
+    if (!isValid) {
+      return;
+    }
+    const totalCost = formatPrice(calculateTotal(tripCost, options));
 
-  fetch(url, fetchOptions)
-    .then(function(response){
-      return response.json();
-    }).then(function(parsedResponse){
-      console.log('parsedResponse', parsedResponse);
-    });
-};
+    const payload = {
+      ...options,
+      totalCost,
+      tripId,
+      tripName,
+      countryCode,
+    };
 
-const OrderForm = ({ options, setOrderOption, tripCost, tripId, tripName, countryCode }) => (
-  <Row>
-    {pricing.map(pricingData => (
-      <Col md={4} key={pricingData.id}>
-        <OrderOption
-          currentValue={options[pricingData.id]}
-          setOrderOption={setOrderOption}
-          {...pricingData}
-        />
-      </Col>
-    ))}
-    <Col xs={12}>
-      <OrderSummary tripCost={tripCost} options={options} />
-    </Col>
-    <Button onClick={() => sendOrder(options, tripCost, tripId, tripName, countryCode)}>Order now!</Button>
-  </Row>
-);
+    const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    fetch(url, fetchOptions)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+      });
+  }
+
+  render() {
+    const {
+      options,
+      setOrderOption,
+      tripCost,
+      tripId,
+      tripName,
+      countryCode,
+    } = this.props;
+    return (
+      <Row>
+        {pricing.map(pricingData => (
+          <Col md={4} key={pricingData.id}>
+            <OrderOption
+              currentValue={options[pricingData.id]}
+              setOrderOption={setOrderOption}
+              {...pricingData}
+            />
+          </Col>
+        ))}
+        <Col xs={12}>
+          <OrderSummary tripCost={tripCost} options={options} />
+        </Col>
+        <Button
+          onClick={() =>
+            this.sendOrder(options, tripCost, tripId, tripName, countryCode)
+          }
+        >
+          Order now!
+        </Button>
+        {this.state.error && <p>{this.state.error}</p>}
+      </Row>
+    );
+  }
+}
 
 OrderForm.propTypes = {
   tripCost: PropTypes.node,
