532cb7f4f13f91355c2508fedf9cf7655aa433cf
Angelika Draczynska <nayrissa@outlook.com>
2019-11-22 21:20:54 +0100

    ex 20.4

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,4 +1,5 @@
 import { formatTime } from './formatTime';
+import { promoPrice } from './promoPrice';
 
 describe('utils', () => {
   describe('formatTime', () => {
@@ -21,6 +22,26 @@ describe('utils', () => {
       expect(formatTime(3793)).toBe('01:03:13');
       expect(formatTime(120)).toBe('00:02:00');
       expect(formatTime(3604)).toBe('01:00:04');
+    });
+  });
+
+  describe('promoPrice', () => {
+    it('should return null if there is no arg', () => {
+      expect(promoPrice()).toBe(null);
+    });
+
+    it('should return null if arg is not a number', () => {
+      expect(promoPrice('abc')).toBe(null);
+      expect(promoPrice(() => {})).toBe(null);
+    });
+
+    it('should return null if arg is lower than zero', () => {
+      expect(promoPrice(-1)).toBe(null);
+      expect(promoPrice(-2)).toBe(null);
+    });
+
+    it('should return time in hh:mm:ss if arg is proper', () => {
+      expect(promoPrice(50, 20)).toBe(10);
     });
   });
 });
